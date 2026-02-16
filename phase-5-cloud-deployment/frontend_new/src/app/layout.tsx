51bc4c1ee46dab32b37f2490cc60ddb4e5c1c6b4
kristinasamuel <166210993+kristinasamuel@users.noreply.github.com>
2026-02-16 13:34:28 +0500

    phase-5

--- phase-5-cloud-deployment/frontend_new/src/app/layout.tsx
+++ phase-5-cloud-deployment/frontend_new/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from './Header';
+import ClientHeader from './ClientHeader';
 import Footer from './Footer';
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="min-h-screen flex flex-col">
-          <Header />
+          <ClientHeader />
           <main className="flex-grow">
             {children}
           </main>
